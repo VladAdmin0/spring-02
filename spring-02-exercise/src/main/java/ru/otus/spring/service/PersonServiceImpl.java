b558bdecf0983ca27a683a6b572128d155cd267f
VladAdmin0 <vladimirsmir.1@gmail.com>
2023-08-13 10:42:31 +0300

    first

--- spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
+++ spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
@@ -1,13 +1,16 @@
 package ru.otus.spring.service;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 import ru.otus.spring.dao.PersonDao;
 import ru.otus.spring.domain.Person;
 
+@Service
 public class PersonServiceImpl implements PersonService {
 
     private final PersonDao dao;
 
-    public PersonServiceImpl(PersonDao dao) {
+    public PersonServiceImpl(@Qualifier("personDao") PersonDao dao) {
         this.dao = dao;
     }
 
